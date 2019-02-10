e3747b8a3385b19b717e557bfa859f1339e63fb3
Александр Дебеев <mr.alexander.daa@gmail.com>
2019-02-10 10:37:02 +0600

    вроде ДЗ №15, надо еще глянуть, добавил в test_phone несколько запусков тестов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -23,7 +23,7 @@ test_data_C = [
 
 
 @pytest.mark.parametrize("contact", test_data_C, ids=[repr(x) for x in test_data_C])
-def test_add_contact(app,contact):
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
     app.contact.add_new(contact)
     assert len(old_contacts) + 1 == app.contact.count()
--- test/test_phones.py
+++ test/test_phones.py
@@ -1,19 +1,27 @@
 import  re
 from random import randrange
 from fixture.contact import Contact
+import pytest
+from fixture.contact import ContactHelper
+
+contact1 = Contact(firstname="addbeforechng", middlename="", lastname="", nickname="",
+                   title="", company="", address="", home_phone="1",
+                   mobile_phone="11", work_phone="2", fax="32", email="12@22.qq",
+                   email2="222@22.qq", email3="332@22.qq", homepage="12.kz", bday="7", bmonth="May",
+                   byear="1974", aday="2", amonth="January", ayear="2001", address2="a2, www2",
+                   phone2="132", notes="qwey2",
+                   photo="C:\_users\Alexander\__kurs\gitP\python_tr\photo.png")
 
 
-def test_phones_on_home_page(app):
+test_data_ph = [i for i in range(2)]
+
+
+@pytest.mark.parametrize("index", test_data_ph)
+def test_phones_on_home_page(app, index):
     if app.contact.count() == 0:
-        app.contact.add_new(Contact(firstname="addbeforechng", middlename="", lastname="", nickname="",
-                                title="", company="", address="", home_phone="1",
-                                mobile_phone="11", work_phone="2", fax="32", email="12@22.qq",
-                                email2="222@22.qq", email3="332@22.qq", homepage="12.kz", bday="7", bmonth="May",
-                                byear="1974", aday="2", amonth="January", ayear="2001", address2="a2, www2",
-                                phone2="132", notes="qwey2",
-                                photo="C:\_users\Alexander\__kurs\gitP\python_tr\photo.png"))
+        app.contact.add_new(contact1)
     index = randrange(app.contact.count())
-    #print(str(index))
+    print(str(index))
     contact_from_home_page = app.contact.get_contact_list()[index]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
@@ -22,18 +30,12 @@ def test_phones_on_home_page(app):
     assert contact_from_home_page.address == contact_from_edit_page.address
     assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
 
-
-def test_phones_on_contact_view_page(app):
+@pytest.mark.parametrize("q", test_data_ph)
+def test_phones_on_contact_view_page(app,q):
     if app.contact.count() == 0:
-        app.contact.add_new(Contact(firstname="addbeforechng", middlename="", lastname="", nickname="",
-                                title="", company="", address="", home_phone="1",
-                                mobile_phone="11", work_phone="2", fax="32", email="12@22.qq",
-                                email2="222@22.qq", email3="332@22.qq", homepage="12.kz", bday="7", bmonth="May",
-                                byear="1974", aday="2", amonth="January", ayear="2001", address2="a2, www2",
-                                phone2="132", notes="qwey2",
-                                photo="C:\_users\Alexander\__kurs\gitP\python_tr\photo.png"))
+        app.contact.add_new(contact1)
     index = randrange(app.contact.count())
-    #print(str(index))
+    print(str(index))
     contact_from_view_page = app.contact.get_contact_from_view_page(index)
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
     assert contact_from_view_page.home_phone == contact_from_edit_page.home_phone
