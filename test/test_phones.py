8b57de55f5ca4a5d00703f08ae63506cd720def6
AlexanderDaa <mr.alexander.daa@gmail.com>
2019-02-26 23:56:45 +0600

    ДЗ 21 в test_phones.py создан тест test_all_phones_on_home_page, для проверки информации о контактах на главной странице, реализовано сравнение для всех записей, сравнивается с информацией из бд

--- test/test_phones.py
+++ test/test_phones.py
@@ -3,6 +3,7 @@ import re
 from random import randrange
 from fixture.contact import Contact
 import os.path
+from fixture.orm import ORMFixture
 
 
 contact1 = Contact(firstname="addbeforechng", middlename="", lastname="", nickname="",
@@ -57,3 +58,43 @@ def merge_emails_like_on_home_page(contact):
                             map(lambda x: clear(x),
                                 filter(lambda x: x is not None,
                                     ([contact.email, contact.email2, contact.email3])))))
+
+
+def test_all_phones_on_home_page(app):
+    db = ORMFixture(host = "127.0.0.1", name = "addressbook", user = "root", password = "")
+    if len(db.get_contact_list()) == 0:
+        app.contact.add_new(contact1)
+    contact_list_fm_db = sorted(db.get_contact_list(), key=Contact.id_or_max)
+    contact_list_fm_hp = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
+    #print(contact_list_fm_db)
+    #print(contact_list_fm_hp)
+    assert contact_list_fm_db == contact_list_fm_hp
+    ii=-1
+    for i in contact_list_fm_db:
+        ii=ii+1
+        #print(i.home_phone, " ", i.work_phone, " ", i.mobile_phone," ",i.phone2)
+        #print(i.email, " ",i.email2, " ",i.email3)
+        #print(i)
+        #print(contact_list_fm_hp[ii])
+        assert contact_list_fm_hp[ii].firstname == i.firstname
+        assert contact_list_fm_hp[ii].lastname == i.lastname
+        assert contact_list_fm_hp[ii].address == i.address
+        #print(contact_list_fm_hp[ii].all_emails_from_home_page)
+        #print(i.email,'\n',i.email2,'\n',i.email3)
+        #print(merge_emails_like_on_home_page(i))
+        #print(contact_list_fm_hp[ii].all_phones_from_home_page)
+        assert contact_list_fm_hp[ii].all_emails_from_home_page == merge_emails_like_on_home_page(i)
+        assert contact_list_fm_hp[ii].all_phones_from_home_page == merge_phones_like_on_home_page(i)
+    print("%s contacts checked" % ii)
+
+#    index = randrange(app.contact.count())
+    #print(str(index))
+#    contact_from_home_page = app.contact.get_contact_list()[index]
+#    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
+
+#    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
+#    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
+#    assert contact_from_home_page.address == contact_from_edit_page.address
+
+#    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+#    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
