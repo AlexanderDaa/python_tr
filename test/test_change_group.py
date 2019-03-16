1b17f379333eed296198245a074805c798c38002
AlexanderDaa <mr.alexander.daa@gmail.com>
2019-03-16 11:40:40 +0600

    разметка для allure сделана для тестов для добавления, удаления и изменения групп

--- test/test_change_group.py
+++ test/test_change_group.py
@@ -5,18 +5,22 @@ import pytest
 
 def test_modify_group(app, db, check_ui, json_groups):
     group = json_groups
-    if app.group.count() == 0:
-        app.group.create(Group(name="a", header="b", footer="c"))
-    old_groups = db.get_group_list()
-    group_to_ch = random.choice(old_groups)
-    app.group.change_group_by_id(group_to_ch.id,group)
-    new_groups = db.get_group_list()
-    group.id = group_to_ch.id
-    old_groups[old_groups.index(group_to_ch)]=group
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-    if check_ui:
-        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
-        print("check_ui")
+    with pytest.allure.step('Given a non-empty group list'):
+        if app.group.count() == 0:
+            app.group.create(Group(name="a", header="b", footer="c"))
+        old_groups = db.get_group_list()
+    with pytest.allure.step('Given a random group from the list'):
+        group_to_ch = random.choice(old_groups)
+    with pytest.allure.step('When I change the group %s' % group_to_ch):
+        app.group.change_group_by_id(group_to_ch.id,group)
+    with pytest.allure.step('Then the new list is equal to the old list with changed group'):
+        new_groups = db.get_group_list()
+        group.id = group_to_ch.id
+        old_groups[old_groups.index(group_to_ch)]=group
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+        if check_ui:
+            assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+            print("check_ui")
 
 
 
